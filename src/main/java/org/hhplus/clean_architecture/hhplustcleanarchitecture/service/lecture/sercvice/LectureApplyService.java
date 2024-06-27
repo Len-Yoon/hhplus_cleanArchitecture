a282f92bad46d5fa6af3b9805c13f44bf209585f
Len <mabest123@naver.com>
2024-06-27 19:13:03 +0900

    Step3 과제 return 부분 수정

--- src/main/java/org/hhplus/clean_architecture/hhplustcleanarchitecture/service/lecture/sercvice/LectureApplyService.java
+++ src/main/java/org/hhplus/clean_architecture/hhplustcleanarchitecture/service/lecture/sercvice/LectureApplyService.java
@@ -1,17 +1,18 @@
 package org.hhplus.clean_architecture.hhplustcleanarchitecture.service.lecture.sercvice;
 
+import org.hhplus.clean_architecture.hhplustcleanarchitecture.service.lecture.dto.LectureHistory;
 import org.hhplus.clean_architecture.hhplustcleanarchitecture.service.lecture.dto.Lectures;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 public interface LectureApplyService {
 
     //특강 목록 조회
-    void checkLectures();
+    List<Lectures> checkLectures();
     //특강 신청
     void applyLecture(int lectureId, int userId);
     //특강 신청 결과 여부 조회
     boolean isApplyComplete(int lectureId, int userId);
     //특강 신청 리스트 조회
-    void appliedList(int userId);
+    List<LectureHistory> appliedList(int userId);
 }
